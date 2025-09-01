9d9c8c365c622fda93d371428ce240619f54e461
phoenix <phoenix@ThinkPad.lan>
2025-09-01 09:18:14 +0200

    ok

---
+++ 01/ex05/Harl.cpp
@@ -0,0 +1,44 @@
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+void Harl::info(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+void Harl::warning(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+void Harl::complain(std::string level)
+{
+	void (Harl::*functions[])(void) = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error};
+
+	std::string levels[] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*functions[i])();
+			return;
+		}
+	}
+
+	std::cout << "No level named: " << level << std::endl;
+}
