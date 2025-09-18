34c60fffed60e026ef9ad498a2d802cac163cb18
Your Name <you@example.com>
2025-09-19 00:28:38 +0200

    xd

--- 04/ex02/main.cpp
+++ 04/ex02/main.cpp
@@ -1,45 +1,47 @@
 #include "Brain.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 int main()
 {
-	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog("Dog");
-	// const Animal* i = new Cat("Cat");
+	//const Animal* meta = new Animal();
+	const Animal* j = new Dog("Dog");
+	const Animal* i = new Cat("Cat");
 	
 
-	// std::cout << "-------GOOD POLYMORPHISM DISPLAY-------" << std::endl;
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound();
-	// j->makeSound();
-	// meta->makeSound();
-	// std::cout << "-------GOOD POLYMORPHISM DISPLAY-------" << std::endl;
+	std::cout << "-------GOOD POLYMORPHISM DISPLAY-------" << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	//meta->makeSound();
+	std::cout << "-------GOOD POLYMORPHISM DISPLAY-------" << std::endl;
 
-	// std::cout << "-------BAD POLYMORPHISM DISPLAY-------" << std::endl;
-	// const WrongAnimal *wa = new WrongAnimal();
-	// const WrongAnimal *wc = new WrongCat();
-	// wa->makeSound();
-	// wc->makeSound();
-	// std::cout << "-------BAD POLYMORPHISM DISPLAY-------" << std::endl;
+	std::cout << "-------BAD POLYMORPHISM DISPLAY-------" << std::endl;
+	const WrongAnimal *wa = new WrongAnimal();
+	const WrongAnimal *wc = new WrongCat();
+	wa->makeSound();
+	wc->makeSound();
+	std::cout << "-------BAD POLYMORPHISM DISPLAY-------" << std::endl;
 
 
-	// delete meta, delete j, delete i;
-	// delete wa, delete wc;
-	// Animal **animalArr = new Animal*[10];
-	// for(size_t i = 0; i < 10; i++)
-	// {
-	// 	if(i == 0)
-	// 		animalArr[i] = new Dog();
-	// 	else if(i % 2 == 0)
-	// 		animalArr[i] = new Cat();
-	// 	else
-	// 		animalArr[i] = new Dog();
-	// }
-	// for(size_t i = 0; i < 10; i++)
-	// 	delete animalArr[i];
-	// delete[] animalArr;
-	// Brain *b = new Brain();
-	// delete(b);
-	return 0;
+	delete j; delete i;
+	delete wa; delete wc;
+// 	Animal **animalArr = new Animal*[10];
+// 	for(size_t i = 0; i < 10; i++)
+// 	{
+// 		if(i == 0)
+// 			animalArr[i] = new Dog();
+// 		else if(i % 2 == 0)
+// 			animalArr[i] = new Cat();
+// 		else
+// 			animalArr[i] = new Dog();
+// 	}
+// 	for(size_t i = 0; i < 10; i++)
+// 		delete animalArr[i];
+// 	delete[] animalArr;
+// 	Brain *b = new Brain();
+// 	delete(b);
+// 	return 0;
 }
